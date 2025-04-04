6ed7148836bac02bc148fdd7b37958bbb56f7a0e
Felipe Silva <felipe.silva@webtech87.pt>
2025-04-04 09:38:52 +0100

    [FIX] Reset DB and updated environment for deploy

--- backend/build.sh
+++ backend/build.sh
@@ -8,10 +8,6 @@ cd backend
 # Install dependencies
 pip install -r requirements.txt
 
-# Fake initial migrations if needed to fix order issue
-python manage.py migrate --fake users
-python manage.py migrate --fake admin
-
 # Apply all migrations properly
 python manage.py migrate
 
