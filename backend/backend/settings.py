a5997109da7faa0083f837e742423c8152485596
Felipe Silva <felipe.silva@webtech87.pt>
2025-04-05 13:11:13 +0100

    [FIX] Add STATICFILES_STORAGE for Whitenoise

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -205,6 +205,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
