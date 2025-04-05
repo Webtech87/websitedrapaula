2decf3b6a68a5b80fc384c5cf6862d30cf916b6a
Felipe Silva <felipe.silva@webtech87.pt>
2025-04-05 14:16:06 +0100

    [FIX] Change all occurrences of localhost IP to Render's URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path"; // <-- Add this import
 
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
