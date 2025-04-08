627afe54d3a0082a261ad382e104b615e4e85a9a
Felipe Silva <felipe.silva@webtech87.pt>
2025-04-08 18:39:36 +0100

    [FIX] Correct mispelling on css imports

--- frontend/src/components/PoliticaCookies.tsx
+++ frontend/src/components/PoliticaCookies.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/politicaCookies.css";
+import "../styles/PoliticaCookies.css";
 
 const PoliticaCookies = () => {
   return (
--- frontend/src/components/TermosCondicoes.tsx
+++ frontend/src/components/TermosCondicoes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/termosCondicoes.css";
+import "../styles/TermosCondicoes.css";
 
 const TermosCondicoes = () => {
   return (
